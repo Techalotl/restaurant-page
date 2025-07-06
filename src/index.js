3960e531953f146ce2f10f11ad329bc0fa3b5ca8
Adriana Coll <169408160+Techalotl@users.noreply.github.com>
2025-07-05 19:01:13 -0600

    Add temporal listeners for the buttons

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import "./pages/home.js";
+
+const logo = document.querySelector(".logo");
+const home = document.querySelector("#home");
+const menu = document.querySelector("#menu");
+const about = document.querySelector("#about");
+
+home.addEventListener("click", () => {
+    console.log("home page clicked")
+});
+
+menu.addEventListener("click", () => {
+    console.log("menu was clicked")
+});
+
+about.addEventListener("click", () => {
+    console.log("about page was clicked")
+});
+
+logo.addEventListener("click", () => {
+    console.log("logo was clicked")
+});
